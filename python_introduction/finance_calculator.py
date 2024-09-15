da921e2e696c0bf16bd74fce8e5299acfaa2bf62
Yvonne Bosire <yvonnebosire24@gmail.com>
2024-09-15 16:18:21 +0300

    Add personal finance calculator script

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -14,3 +14,4 @@ projected_savings = annual_savings + (annual_savings * 0.05)
 # Display the results
 print(f"Your monthly savings are ${monthly_savings:.2f}.")
 print(f"Projected savings after one year, with interest, is: ${projected_savings:.2f}.")
+
