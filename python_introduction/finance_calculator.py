f90b1040f161d200f7bbe09ff9edb2f99058b57c
Yvonne Bosire <yvonnebosire24@gmail.com>
2024-09-15 16:32:09 +0300

    Fix variable name in finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -5,7 +5,7 @@ monthly_income = float(input("Enter your monthly income: "))
 total_expenses = float(input("Enter your total monthly expenses: "))
 
 # Calculate monthly savings
-monthly_savings = monthly_income - total_expenses
+monthly_savings = monthly_income - monthly_expenses
 
 # Project annual savings with a 5% interest rate
 annual_savings = monthly_savings * 12
