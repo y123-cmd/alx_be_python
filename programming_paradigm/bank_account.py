f60b8ba7974a986a84f411d2ce4f34eb62925d0e
Yvonne Bosire <yvonnebosire24@gmail.com>
2024-10-07 12:23:05 +0300

    updated

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,4 +15,4 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
